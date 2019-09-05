28806d0f82a7cba41ff7e174dfa390e1f831d634
elftvxq <emmashih721@hotmail.com>
2019-09-06 01:43:13 +0800

    express set up and sign up auth

--- functions/index.js
+++ functions/index.js
@@ -1,26 +1,114 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const app = require('express')();
 
 admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+const config = {
+    apiKey: "AIzaSyCCd98pmJINn6EnfUvW66xF_oJPBbZZ-TM",
+    authDomain: "react-personal-project.firebaseapp.com",
+    databaseURL: "https://react-personal-project.firebaseio.com",
+    projectId: "react-personal-project",
+    storageBucket: "react-personal-project.appspot.com",
+    messagingSenderId: "1066658697188",
+};
+
+const firebase= require('firebase');
+firebase.initializeApp(config);
+
+const db = admin.firestore();
+
+
+//第一個參數為路由
+app.get('/screams', (req, res) =>{
+    db
+        .collection('screams')
+        .orderBy('createdAt', 'desc')
+        .get()
+        .then((data) => {
+            let screams = [];
+            data.forEach((doc) => {
+                screams.push({
+                    screamId: doc.id,
+                    body: doc.data().body,
+                    userHandle: doc.data().userHandle,
+                    createdAt: doc.data().createdAt
+                })
+            });
+            return res.json(screams);
+        })
+        .catch((err) => console.error(err));
+})
+
+
+//post one scream
+app.post('/scream', (req, res) => {
+    
+    const newScream = {
+        body: req.body.body,
+        userHandle: req.body.userHandle,
+        createdAt: new Date().toISOString()
+    };
+
+    db
+        .collection('screams') 
+        .add(newScream)
+        .then((doc) => {
+            res.json({ message: `document ${doc.id} created successfully` })
+        })
+        .catch((err) => {
+            res.status(500).json({ error: 'something went wrong' })
+            console.error(err);
+        })
+}); 
+
+
+// Signup route
+app.post('/signup', (req, res) => {
+    const newUser = {
+        email: req.body.email,
+        password: req.body.password,
+        confirmPassword: req.body.confirmPassword,
+        handle: req.body.handle,
+    };
+
+    // TODO: validate data
+    let token, userId;
+    db.doc(`/users/${newUser.handle}`).get()
+      .then((doc)=>{
+          if (doc.exists) {
+            return res.status(400).json({ handle: 'this handle is already taken' });   
+          } else {
+              return firebase
+              .auth()
+              .createUserWithEmailAndPassword(newUser.email, newUser.password)
+          }
+      })
+      .then((data)=> {
+          userId = data.user.uid;
+          return data.user.getIdToken();
+      })  
+      .then((idToken) =>{
+          token = idToken;
+          const userCredentials = {
+              handle: newUser.handle,
+              email: newUser.email,
+              createdAt: new Date().toISOString(),
+              userId
+          };
+          return db.doc(`/users/${newUser.handle}`).set(userCredentials);
+      })
+      .then(()=> {
+        return res.status(201).json({ token });
+      })
+      .catch((err) =>{
+          console.error(err);
+          if(err.code === 'auth/email-already-in-use'){
+              return res.status(400).json({ email: 'Email is already use'})
+          } else{
+              return res.status(500).json({ error: err.code })
+          }
+      });
 });
 
-exports.getScreams = functions.https.onRequest((req, res) => {
-    admin
-    .firestore()
-    .collection('screams')
-    .get()
-    .then((data) => {
-        let screams = [];
-        data.forEach((doc) => {
-            screams.push(doc.data())
-        });
-        return res.json(screams);
-    })
-    .catch((err) => console.error(err))
-}); 
+exports.api = functions.https.onRequest(app);
