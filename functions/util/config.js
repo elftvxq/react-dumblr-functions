fdf357ba8f2c34c47a23ed2d1f7d6a12e97a0e43
施琬妤 <shiwanyu@shiwanyus-MacBook-Pro.local>
2019-09-06 17:06:33 +0800

    seperate and manage the function files

---
+++ functions/util/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    apiKey: "AIzaSyCCd98pmJINn6EnfUvW66xF_oJPBbZZ-TM",
+    authDomain: "react-personal-project.firebaseapp.com",
+    databaseURL: "https://react-personal-project.firebaseio.com",
+    projectId: "react-personal-project",
+    storageBucket: "react-personal-project.appspot.com",
+    messagingSenderId: "1066658697188",
+};
