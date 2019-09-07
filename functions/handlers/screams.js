001202a8f551c0c107929862bc5414ae23c8fd5e
施琬妤 <shiwanyu@shiwanyus-MacBook-Pro.local>
2019-09-08 02:15:52 +0800

    save the index.js

--- functions/handlers/screams.js
+++ functions/handlers/screams.js
@@ -177,7 +177,8 @@ exports.unlikeScream = (req, res) =>{
         if(data.empty){
             return res.status(400).json({ error: 'Scream not liked'});
         } else {
-            return db.doc(`/likes/${data.docs[0].data().id}`).delete()
+            return db.doc(`/likes/${data.docs[0].data().id}`)
+            .delete()
             .then(() =>{
                 screamData.likeCount--;
                 return screamDocument.update({ likeCount: screamData.likeCount});
--- functions/index.js
+++ functions/index.js
@@ -26,7 +26,7 @@ app.post('/scream', FBAuth, postOneScream);
 app.get('/scream/:screamId', getScream);
 // TODO delete scream
 app.get('/scream/:screamId/like', FBAuth, likeScream);
-app.get('/scream/:screamId/unlike', FBAuth, unlikeScream);
+// app.get('/scream/:screamId/unlike', FBAuth, unlikeScream);
 app.post('/scream/:screamId/comment', FBAuth, commentOnScream);
 
 
