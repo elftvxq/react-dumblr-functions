fd59b15132b18fcb2fc958fb763d00720716aa03
施琬妤 <shiwanyu@shiwanyus-MacBook-Pro.local>
2019-09-08 11:25:14 +0800

    able to delete the post from database

--- functions/handlers/screams.js
+++ functions/handlers/screams.js
@@ -98,6 +98,9 @@ exports.commentOnScream = (req, res) =>{
        if (!doc.exists) {
            return res.status(404).json({ error: 'Scream notfound'});
        }
+       return doc.ref.update({ commentCount: doc.data().commentCount + 1});
+   })
+   .then(()=>{
        return db.collection('comments').add(newComment);
    })
    .then(() =>{
@@ -177,7 +180,7 @@ exports.unlikeScream = (req, res) =>{
         if(data.empty){
             return res.status(400).json({ error: 'Scream not liked'});
         } else {
-            return db.doc(`/likes/${data.docs[0].data().id}`)
+            return db.doc(`/likes/${data.docs[0].id}`)
             .delete()
             .then(() =>{
                 screamData.likeCount--;
@@ -192,4 +195,28 @@ exports.unlikeScream = (req, res) =>{
         console.error(err);
         res.status(500).json({ error: err.code });
     }); 
-}
+};
+
+//Delete a scream
+
+exports.deleteScream = (req, res) =>{
+    const document = db.doc(`/screams/${req.params.screamId}`);
+    document.get()
+    .then((doc) => {
+        if(!doc.exists){
+            return res.status(400).json({ error: 'Scream not found'});
+        }
+        if(doc.data().userHandle !== req.user.handle){
+            return res.status(403).json({ error: 'Unauthorized'});
+        } else {
+            return document.delete();
+        }
+    })
+    .then(()=> {
+        res.json({ message: 'Scream deleted successfully'});
+    })
+    .catch((err) => {
+        console.error(err);
+        return res.status(500).json({ error: err.code });
+    });
+};
