5a49910f56ababc6bc639f695f6b80638dfaa0a1
施琬妤 <shiwanyu@shiwanyus-MacBook-Pro.local>
2019-09-08 15:32:09 +0800

    change profile image and immediately update, delete the post w/ like

--- functions/handlers/screams.js
+++ functions/handlers/screams.js
@@ -12,7 +12,10 @@ exports.getAllScreams = (req, res) => {
                     screamId: doc.id,
                     body: doc.data().body,
                     userHandle: doc.data().userHandle,
-                    createdAt: doc.data().createdAt
+                    createdAt: doc.data().createdAt,
+                    commentCount: doc.data().commentCount,
+                    likeCount: doc.data().likeCount,
+                    userImage: doc.data().userImage
                 })
             });
             return res.json(screams);
@@ -83,7 +86,7 @@ exports.getScream = (req, res) => {
 
 //Comment on a scream
 exports.commentOnScream = (req, res) =>{
-   if(req.body.body.trim() === '') return res.status(400).json({ error: 'Comment not be empty' });
+   if(req.body.body.trim() === '') return res.status(400).json({ comment: 'Comment not be empty' });
    
    const newComment = {
        body: req.body.body,
@@ -198,7 +201,6 @@ exports.unlikeScream = (req, res) =>{
 };
 
 //Delete a scream
-
 exports.deleteScream = (req, res) =>{
     const document = db.doc(`/screams/${req.params.screamId}`);
     document.get()
